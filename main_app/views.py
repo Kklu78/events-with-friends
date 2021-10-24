a1765482c899cd6655df9b34dafe1f4eb961b89f
alondra143 <aguzman7029@gmail.com>
2021-10-24 10:40:39 -0700

    fixed templates

--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,21 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required #functionbased
 from django.contrib.auth.mixins import LoginRequiredMixin #classbased
+from dotenv import load_dotenv
+import os
+import requests
+import pprint
+load_dotenv()
+TM_CONSUMER_KEY = os.getenv("TM_CONSUMER_KEY")
+TM_CONSUMER_SECRET_KEY = os.getenv("TM_CONSUMER_SECRET_KEY")
 
+city = 'Los Angeles'
+state = 'CA'
+size = 2
+api_url = f'https://app.ticketmaster.com/discovery/v2/events.json?size={size}&city={city}&stateCode={state}&apikey={TM_CONSUMER_KEY}'
+# print(api_url)
+r = requests.get(api_url)
+# pprint.pprint(r.json())
 # just in case/for aws thing if needed
 # import uuid #random numbers for urls
 # import boto3 to talk to aws
