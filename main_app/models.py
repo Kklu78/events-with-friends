42e791c2548f7c161a328f4a8cbd5ef47fb98675
alondra143 <aguzman7029@gmail.com>
2021-10-25 13:18:25 -0700

    name in model

--- main_app/models.py
+++ main_app/models.py
@@ -15,6 +15,7 @@ LOCATIONS = (
 
 
 class Event(models.Model):
+    name = models.CharField(max_length=100)
     event_id = models.CharField(max_length=50)
     def __str__(self):
         return self.name
